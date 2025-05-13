70616d78c04c18b28790ea553cae5b772ceb0460
Marzuk Kamal <marzuk.kamal@aeonx.ai>
2025-05-13 23:51:27 +0200

    code added

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import { StorageEnum, StorageType } from "./types";
 
 
 
-export function getRandomInt(max) {
+export function getRandomInt(max: number): number {
   return Math.floor(Math.random() * max) *1000;
 }
 
